77d1c526a080934a313a806a2601a6656d31cbab
Florian Hussonnois <florian.hussonnois@gmail.com>
2021-10-11 21:12:42 +0200

    refactor(all): remove unused methods and deps

--- src/main/java/io/streamthoughts/kafka/specs/CLIUtils.java
+++ src/main/java/io/streamthoughts/kafka/specs/CLIUtils.java
@@ -18,51 +18,15 @@
  */
 package io.streamthoughts.kafka.specs;
 
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-import joptsimple.OptionSpec;
 import org.apache.kafka.common.utils.Exit;
 import picocli.CommandLine;
 
 import java.io.Console;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Utility class to handle arguments.
  */
 public class CLIUtils {
-
-    static void checkRequiredArgs(final OptionParser parser, final OptionSet options, final OptionSpec... required) {
-        for (OptionSpec arg : required) {
-            if(!options.has(arg))
-                printUsageAndDie(parser, "Missing required argument \"" + arg + "\"");
-        }
-    }
-
-    static void checkOnceRequiredArgs(final OptionParser parser, final OptionSet options, final OptionSpec<String>... required) {
-        Set<String> names = new HashSet<>();
-        for (OptionSpec<String> arg : required) {
-            if (!options.has(arg)) {
-                List<String> flags = arg.options();
-                names.add(flags.get(0));
-            }
-        }
-        if (names.size() == required.length) {
-            printUsageAndDie(parser, "Missing required argument : \"" + names + "\"");
-        }
-    }
-
-    static void printUsageAndDie(final OptionParser parser, final String message) {
-        System.err.println(message);
-        try {
-            parser.printHelpOn(System.err);
-        } catch (IOException ignore) {
-        }
-        System.exit(1);
-    }
 
     public static void askToProceed(final CommandLine.Model.CommandSpec spec) {
         final String description = spec.commandLine().getHelp().description();
