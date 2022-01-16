8eb6ddad79add8a47ceb3a6d02a66416e416aa76
Florian Hussonnois <florian.hussonnois@gmail.com>
2022-01-16 23:44:36 +0100

    refactor(jikkou): remove unused method

--- src/main/java/io/streamthoughts/kafka/specs/command/broker/subcommands/Describe.java
+++ src/main/java/io/streamthoughts/kafka/specs/command/broker/subcommands/Describe.java
@@ -20,7 +20,6 @@ package io.streamthoughts.kafka.specs.command.broker.subcommands;
 
 import io.streamthoughts.kafka.specs.command.BaseCommand;
 import io.streamthoughts.kafka.specs.config.JikkouConfig;
-import io.streamthoughts.kafka.specs.internal.KafkaUtils;
 import io.streamthoughts.kafka.specs.io.YAMLSpecWriter;
 import io.streamthoughts.kafka.specs.manager.BrokerDescribeOptions;
 import io.streamthoughts.kafka.specs.manager.KafkaBrokerManager;
@@ -29,8 +28,6 @@ import io.streamthoughts.kafka.specs.model.MetaObject;
 import io.streamthoughts.kafka.specs.model.V1BrokerObject;
 import io.streamthoughts.kafka.specs.model.V1SpecFile;
 import io.streamthoughts.kafka.specs.model.V1SpecsObject;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.common.Node;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -39,10 +36,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 @Command(name = "describe",
         description = "Describe all the Broker's configuration on remote cluster."
@@ -96,12 +90,5 @@ public class Describe extends BaseCommand {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private Collection<String> loadClusterBrokerIds(final AdminClient client) {
-        CompletableFuture<Collection<Node>> topics = KafkaUtils.listBrokers(client);
-        return topics
-                .thenApply(t -> t.stream().map(Node::idString).collect(Collectors.toList()))
-                .join();
     }
 }
