7252ff17b4ebd8929132185f3c0ea5422f7ec15e
Florian Hussonnois <florian.hussonnois@gmail.com>
2022-04-21 16:36:54 +0200

    refactor: add additional constructors to AdminClientContext class

--- src/main/java/io/streamthoughts/kafka/specs/manager/adminclient/AdminClientContext.java
+++ src/main/java/io/streamthoughts/kafka/specs/manager/adminclient/AdminClientContext.java
@@ -20,6 +20,7 @@ package io.streamthoughts.kafka.specs.manager.adminclient;
 
 import io.streamthoughts.kafka.specs.config.JikkouConfig;
 import io.streamthoughts.kafka.specs.config.JikkouParams;
+import io.streamthoughts.kafka.specs.error.ConfigException;
 import io.streamthoughts.kafka.specs.error.JikkouException;
 import io.streamthoughts.kafka.specs.internal.KafkaBrokersReady;
 import io.streamthoughts.kafka.specs.internal.KafkaUtils;
@@ -27,36 +28,102 @@ import org.apache.kafka.clients.admin.AdminClient;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.Properties;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
+/**
+ * Class for executing operation using an ephemeral {@link AdminClient} instance.
+ */
 public class AdminClientContext {
+
+    private static final int DEFAULT_MIN_AVAILABLE_BROKERS = 1;
+    private static final long DEFAULT_TIMEOUT_MS = 60000L;
+    private static final long DEFAULT_RETRY_BACKOFF_MS = 1000L;
 
     public interface KafkaFunction<O> extends Function<AdminClient, O> { }
 
-    private final JikkouConfig config;
+    private final Supplier<AdminClient> adminClientSupplier;
 
     private AdminClient client;
 
+    private KafkaBrokersReady.Options options = KafkaBrokersReady.Options.withDefaults()
+            .withTimeoutMs(DEFAULT_TIMEOUT_MS)
+            .withMinAvailableBrokers(DEFAULT_MIN_AVAILABLE_BROKERS)
+            .withRetryBackoffMs(DEFAULT_RETRY_BACKOFF_MS);
+
+    private boolean isWaitForKafkaBrokersEnabled = true;
+
     /**
-     * Creates a new {@link AdminClientContext} instance.
+     * Creates a new {@link AdminClientContext} instance with the specified {@link AdminClient} configuration.
+     *
+     * @param adminClientConfig   the {@link AdminClient} supplier.
+     */
+    public AdminClientContext(final @NotNull Properties adminClientConfig) {
+        this(() -> KafkaUtils.newAdminClient(adminClientConfig));
+    }
+
+    /**
+     * Creates a new {@link AdminClientContext} instance with the specified {@link AdminClient} supplier.
+     *
+     * @param adminClientSupplier   the {@link AdminClient} supplier.
+     */
+    public AdminClientContext(final @NotNull Supplier<AdminClient> adminClientSupplier) {
+        this.adminClientSupplier = Objects.requireNonNull(
+                adminClientSupplier,
+                "'adminClientSupplier' should not be null"
+        );
+    }
+
+    /**
+     * Creates a new {@link AdminClientContext} instance with the specified application's configuration.
+     *
      * @param config the {@link JikkouConfig}.
      */
     public AdminClientContext(final @NotNull JikkouConfig config) {
-        this.config = Objects.requireNonNull(config, "'config should not be null'");
+        this(JikkouParams.ADMIN_CLIENT_CONFIG.get(config));
+        withWaitForKafkaBrokersEnabled(JikkouParams.KAFKA_BROKERS_WAIT_FOR_ENABLED.orElseGet(config, () -> true));
+        if (isWaitForKafkaBrokersEnabled) {
+            withWaitForRetryBackoff(JikkouParams.KAFKA_BROKERS_WAIT_FOR_RETRY_BACKOFF_MS.orElseGet(
+                    config,
+                    () -> DEFAULT_RETRY_BACKOFF_MS)
+            );
+            withWaitForMinAvailableBrokers(JikkouParams.KAFKA_BROKERS_WAIT_FOR_MIN_AVAILABLE.orElseGet(
+                    config,
+                    () -> DEFAULT_MIN_AVAILABLE_BROKERS)
+            );
+            withWaitTimeoutMs(JikkouParams.KAFKA_BROKERS_WAIT_FOR_TIMEOUT_MS.orElseGet(
+                    config,
+                    () -> DEFAULT_TIMEOUT_MS)
+            );
+        }
+    }
+
+    public AdminClientContext withWaitForKafkaBrokersEnabled(final boolean isWaitForKafkaBrokersEnabled) {
+        this.isWaitForKafkaBrokersEnabled = isWaitForKafkaBrokersEnabled;
+        return this;
+    }
+
+    public AdminClientContext withWaitForMinAvailableBrokers(final int waitForMinAvailableBrokers) {
+        this.options = options.withMinAvailableBrokers(waitForMinAvailableBrokers);
+        return this;
+    }
+
+    public AdminClientContext withWaitForRetryBackoff(final long waitForRetryBackoff) {
+        this.options = options.withRetryBackoffMs(waitForRetryBackoff);
+        return this;
+    }
+
+    public AdminClientContext withWaitTimeoutMs(final long waitTimeoutMs) {
+        this.options = options.withTimeoutMs(waitTimeoutMs);
+        return this;
     }
 
     public <O> O invokeAndClose(final @NotNull KafkaFunction<O> function) {
-        try (AdminClient client = newAdminClient()) {
+        try (AdminClient client = adminClientSupplier.get()) {
             this.client = client;
-            if (JikkouParams.KAFKA_BROKERS_WAIT_FOR_ENABLED.orElseGet(config, () -> true)) {
-                final boolean isReady = KafkaUtils.waitForKafkaBrokers(
-                        client,
-                        KafkaBrokersReady.Options
-                                .withDefaults()
-                                .withMinAvailableBrokers(JikkouParams.KAFKA_BROKERS_WAIT_FOR_MIN_AVAILABLE.get(config))
-                                .withRetryBackoffMs(JikkouParams.KAFKA_BROKERS_WAIT_FOR_RETRY_BACKOFF_MS.get(config))
-                                .withTimeoutMs(JikkouParams.KAFKA_BROKERS_WAIT_FOR_TIMEOUT_MS.get(config))
-                );
+            if (isWaitForKafkaBrokersEnabled) {
+                final boolean isReady = KafkaUtils.waitForKafkaBrokers(client, options);
                 if (!isReady) {
                     throw new JikkouException(
                             "Timeout expired. The timeout period elapsed prior to " +
@@ -79,16 +146,12 @@ public class AdminClientContext {
      * Retrieve the current {@link AdminClient} instance.
      *
      * @return  the {@link AdminClient}.
-     * @throws  IllegalStateException if not adminClient is currently created.
+     * @throws  IllegalStateException if no AdminClient is currently created.
      */
     public AdminClient current() {
         if (!isInitialized()) {
             throw new IllegalStateException("No current AdminClient created");
         }
         return client;
-    }
-
-    private AdminClient newAdminClient() {
-        return KafkaUtils.newAdminClient(JikkouParams.ADMIN_CLIENT_CONFIG.get(config));
     }
 }
