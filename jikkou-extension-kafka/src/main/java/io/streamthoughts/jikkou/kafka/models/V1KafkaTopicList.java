a909c5ea1b34aa88672cad9759791ac0805dc3d2
Florian Hussonnois <florian.hussonnois@gmail.com>
2023-02-15 17:47:43 +0100

    chore(extension): update header

--- jikkou-extension-kafka/src/main/java/io/streamthoughts/jikkou/kafka/models/V1KafkaTopicList.java
+++ jikkou-extension-kafka/src/main/java/io/streamthoughts/jikkou/kafka/models/V1KafkaTopicList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 StreamThoughts.
+ * Copyright 2023 StreamThoughts.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
