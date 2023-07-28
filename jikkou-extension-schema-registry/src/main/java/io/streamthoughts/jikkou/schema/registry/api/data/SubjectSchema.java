d12f353d2e12783573425825d0e53b638fc3a97d
Brendon Vicente <brendonvicente@gmail.com>
2023-07-28 10:47:32 +0100

    fix(extension-registry): add schema references to data model

--- jikkou-extension-schema-registry/src/main/java/io/streamthoughts/jikkou/schema/registry/api/data/SubjectSchema.java
+++ jikkou-extension-schema-registry/src/main/java/io/streamthoughts/jikkou/schema/registry/api/data/SubjectSchema.java
@@ -18,93 +18,21 @@
  */
 package io.streamthoughts.jikkou.schema.registry.api.data;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Objects;
+import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
 
-public final class SubjectSchema {
+import java.util.List;
 
-    /**
-     * Name of the subject that this schema is registered under
-     */
-    private final String subject;
-
-    /**
-     *  Globally unique identifier of the schema.
-     */
-    private final int id;
-
-    /**
-     * Version of the returned schema.
-     */
-    private final int version;
-
-    /**
-     *  The schema format: AVRO is the default (if no schema type is shown on the response, the type is AVRO), PROTOBUF, JSON
-     */
-    private final String schemaType;
-
-    /**
-     * The schema string
-     */
-    private final String schema;
-
-    @JsonCreator
-    public SubjectSchema(@JsonProperty("subject") String subject,
-                         @JsonProperty("id") int id,
-                         @JsonProperty("version") int version,
-                         @JsonProperty("schemaType") String schemaType,
-                         @JsonProperty("schema") String schema) {
-        this.subject = subject;
-        this.id = id;
-        this.version = version;
-        this.schemaType = schemaType;
-        this.schema = schema;
-    }
-
-    public String subject() {
-        return subject;
-    }
-
-    public int id() {
-        return id;
-    }
-
-    public int version() {
-        return version;
-    }
-
-    public String schemaType() {
-        return schemaType;
-    }
-
-    public String schema() {
-        return schema;
-    }
-
-    /** {@inheritDoc} **/
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SubjectSchema that = (SubjectSchema) o;
-        return id == that.id && version == that.version && Objects.equals(subject, that.subject) && Objects.equals(schemaType, that.schemaType) && Objects.equals(schema, that.schema);
-    }
-
-    /** {@inheritDoc} **/
-    @Override
-    public int hashCode() {
-        return Objects.hash(subject, id, version, schemaType, schema);
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "subject=" + subject +
-                ", id=" + id +
-                ", version=" + version +
-                ", schemaType=" + schemaType +
-                ", schema=" + schema +
-                '}';
-    }
+/**
+ * @param subject    Name of the subject that this schema is registered under
+ * @param id         Globally unique identifier of the schema.
+ * @param version    Version of the returned schema.
+ * @param schemaType The schema format: AVRO is the default (if no schema type is shown on the response, the type is AVRO), PROTOBUF, JSON
+ * @param schema     The schema string
+ * @param references The schema references
+ */
+@Builder
+@Jacksonized
+public record SubjectSchema(String subject, int id, int version, String schemaType, String schema,
+                            List<String> references) {
 }
