3012f328531b6f79da414078c54e256cb35a1af3
Zach Pearce <zach.pearce@acima.com>
2024-05-02 11:27:19 -0600

    fix(schema-registry): Compare avro schemas using fingerprint

--- providers/jikkou-provider-schema-registry/src/main/java/io/streamthoughts/jikkou/schema/registry/model/SchemaAndType.java
+++ providers/jikkou-provider-schema-registry/src/main/java/io/streamthoughts/jikkou/schema/registry/model/SchemaAndType.java
@@ -6,9 +6,18 @@
  */
 package io.streamthoughts.jikkou.schema.registry.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.streamthoughts.jikkou.core.data.json.Json;
+import io.streamthoughts.jikkou.schema.registry.avro.AvroSchema;
 import java.util.Objects;
+import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaNormalization;
 import org.jetbrains.annotations.NotNull;
 
+@Builder
+@Jacksonized
 public class SchemaAndType {
 
     private static final SchemaAndType EMPTY = new SchemaAndType();
@@ -17,6 +26,7 @@ public class SchemaAndType {
         return EMPTY;
     }
 
+    @JsonValue
     private final String schema;
     private final SchemaType type;
 
@@ -39,6 +49,7 @@ public class SchemaAndType {
         this.type = Objects.requireNonNull(type, "type must not be null");
     }
 
+    @JsonValue
     public String schema() {
         return schema;
     }
@@ -53,12 +64,34 @@ public class SchemaAndType {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SchemaAndType that = (SchemaAndType) o;
-        return Objects.equals(schema, that.schema) && type == that.type;
+        if (type == SchemaType.AVRO && type == that.type) {
+            return avroEquals(that);
+        } else if (type == SchemaType.JSON && type == that.type) {
+            return jsonEquals(that);
+        } else {
+            return Objects.equals(schema, that.schema) && type == that.type;
+        }
     }
 
     /** {@inheritDoc} **/
     @Override
     public int hashCode() {
         return Objects.hash(schema, type);
+    }
+
+    private boolean avroEquals(SchemaAndType that) {
+        Schema thisSchema = new AvroSchema(schema).schema();
+        Schema thatSchema = new AvroSchema(that.schema).schema();
+
+        return SchemaNormalization.parsingFingerprint64(thisSchema) ==
+                SchemaNormalization.parsingFingerprint64(thatSchema);
+    }
+
+    private boolean jsonEquals(SchemaAndType that) {
+        return Objects.equals(Json.normalize(schema), Json.normalize(that.schema));
+    }
+
+    public String toString() {
+        return schema;
     }
 }
