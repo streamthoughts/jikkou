a7bc7d5a837f3a4262f91eabf3da4bbee2d4b12f
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-05-31 22:14:26 +0200

    fix(server): update all default ports to 8080

--- server/jikkou-api-server/src/main/docker/entrypoint.sh
+++ server/jikkou-api-server/src/main/docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) The original authors
+#
+# Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+#
 set -e
 
 JIKKOU_CONFIGURATION_PATH=${JIKKOU_CONFIGURATION_PATH:-"/app/resources/"}
