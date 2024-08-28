95b105f4d4de60b18a6ead23efa08bddb01870c2
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-08-28 14:54:15 +0200

    fix(kafka-connect): add missing trace prop to task status (#486)
    Fix: #486

--- providers/jikkou-provider-kafka-connect/src/integration-test/java/io/streamthoughts/jikkou/kafka/connect/reconciler/KafkaConnectorCollectorIT.java
+++ providers/jikkou-provider-kafka-connect/src/integration-test/java/io/streamthoughts/jikkou/kafka/connect/reconciler/KafkaConnectorCollectorIT.java
@@ -149,7 +149,7 @@ class KafkaConnectorCollectorIT extends AbstractKafkaConnectorIT {
                         .withConnectorStatus(new ConnectorStatusResponse(
                                 KAFKA_CONNECTOR_NAME,
                                 new ConnectorStatusResponse.ConnectorStatus("RUNNING", "kafka-connect:8083"),
-                                List.of(new ConnectorStatusResponse.TaskStatus(0, "RUNNING", "kafka-connect:8083"))
+                                List.of(new ConnectorStatusResponse.TaskStatus(0, "RUNNING", "kafka-connect:8083", null))
                         ))
                         .build()
                 )
