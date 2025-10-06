1d3d71b0dfb8821ee6788165131640fe10c76c02
Florian Hussonnois <florian.hussonnois@gmail.com>
2025-10-06 22:18:59 +0200

    fix(core): apply file-pattern config for GitHubResourceRepository

--- providers/jikkou-provider-core/src/test/java/io/streamthoughts/jikkou/core/repository/GitHubResourceRepositoryTest.java
+++ providers/jikkou-provider-core/src/test/java/io/streamthoughts/jikkou/core/repository/GitHubResourceRepositoryTest.java
@@ -31,7 +31,7 @@ class GitHubResourceRepositoryTest {
             public Configuration configuration() {
                 return Configuration.of(
                     GitHubResourceRepository.Config.REPOSITORY_CONFIG.key(), "streamthoughts/jikkou",
-                    GitHubResourceRepository.Config.PATHS_CONFIG.key(), "examples"
+                    GitHubResourceRepository.Config.PATHS_CONFIG.key(), List.of("examples/topic")
                 );
             }
             @Override
@@ -49,6 +49,42 @@ class GitHubResourceRepositoryTest {
 
         // Then
         Assertions.assertNotNull(resources);
-        Assertions.assertFalse(resources.isEmpty());
+        Assertions.assertTrue(resources.size()> 2);
+    }
+
+    @Test
+    void shouldLoadResourceFromPublicGithubRepositoryGivenPattern() {
+        // Given
+        GitHubResourceRepository repository = new GitHubResourceRepository();
+        repository.init(new ExtensionContext() {
+            @Override
+            public String name() {
+                return null;
+            }
+
+            @Override
+            public Configuration configuration() {
+                return Configuration.of(
+                    GitHubResourceRepository.Config.REPOSITORY_CONFIG.key(), "streamthoughts/jikkou",
+                    GitHubResourceRepository.Config.PATHS_CONFIG.key(), List.of("examples/topic"),
+                    GitHubResourceRepository.Config.FILE_PATTERN_CONFIG.key(), "glob:**/kafka-topics.yaml"
+                );
+            }
+            @Override
+            public ExtensionContext contextForExtension(Class<? extends Extension> extension) {
+                return null;
+            }
+
+            @Override
+            public <T extends ExtensionProvider> T provider() {
+                return null;
+            }
+        });
+        // When
+        List<? extends HasMetadata> resources = repository.all();
+
+        // Then
+        Assertions.assertNotNull(resources);
+        Assertions.assertEquals(2, resources.size());
     }
 }
