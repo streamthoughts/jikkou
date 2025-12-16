5746e3832d6d1a12b36f56e58050218526e9520d
Florian Hussonnois <florian.hussonnois@gmail.com>
2025-12-16 23:55:05 +0100

    feat: add support for jinja file locations (#641)
    Adds new 'jikkou.jinja.resourceLocations' property
    to specify file locations from where to allow Jinja to load files.
    
    Fixes: #641

--- template-jinja/src/main/java/io/streamthoughts/jikkou/api/template/JinjaResourceTemplateRenderer.java
+++ template-jinja/src/main/java/io/streamthoughts/jikkou/api/template/JinjaResourceTemplateRenderer.java
@@ -10,6 +10,10 @@ import com.hubspot.jinjava.Jinjava;
 import com.hubspot.jinjava.JinjavaConfig;
 import com.hubspot.jinjava.interpret.RenderResult;
 import com.hubspot.jinjava.interpret.TemplateError;
+import com.hubspot.jinjava.loader.CascadingResourceLocator;
+import com.hubspot.jinjava.loader.ClasspathResourceLocator;
+import com.hubspot.jinjava.loader.FileLocator;
+import com.hubspot.jinjava.loader.ResourceLocator;
 import com.hubspot.jinjava.mode.ExecutionMode;
 import io.streamthoughts.jikkou.common.utils.CollectionUtils;
 import io.streamthoughts.jikkou.core.config.ConfigProperty;
@@ -18,20 +22,27 @@ import io.streamthoughts.jikkou.core.exceptions.ConfigException;
 import io.streamthoughts.jikkou.core.exceptions.JikkouRuntimeException;
 import io.streamthoughts.jikkou.core.template.ResourceTemplateRenderer;
 import io.streamthoughts.jikkou.core.template.TemplateBindings;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * A Jinja based template rendered.
@@ -46,6 +57,11 @@ public class JinjaResourceTemplateRenderer implements ResourceTemplateRenderer {
         .ofBoolean(CONFIG_NS + ".enableRecursiveMacroCalls")
         .defaultValue(true)
         .description("Enable recursive macro calls.");
+
+    public static final ConfigProperty<List<String>> RESOURCE_LOCATIONS_CALLS = ConfigProperty
+        .ofList(CONFIG_NS + ".resourceLocations")
+        .required(false)
+        .description("File locations from where you want to allow Jinja to load files.");
 
     // list of scopes for bindings
     public enum Scopes {
@@ -89,13 +105,27 @@ public class JinjaResourceTemplateRenderer implements ResourceTemplateRenderer {
                          @NotNull final TemplateBindings bindings) {
         LOG.debug("Starting resource template rendering");
         JinjavaConfig config = JinjavaConfig.newBuilder()
-                .withCharset(StandardCharsets.UTF_8)
-                .withFailOnUnknownTokens(failOnUnknownTokens)
-                .withExecutionMode(new PreserveRawExecutionMode(preserveRawTags))
-                .withEnableRecursiveMacroCalls(ENABLE_RECURSIVE_MACRO_CALLS.get(configuration))
-                .build();
+            .withCharset(StandardCharsets.UTF_8)
+            .withFailOnUnknownTokens(failOnUnknownTokens)
+            .withExecutionMode(new PreserveRawExecutionMode(preserveRawTags))
+            .withEnableRecursiveMacroCalls(ENABLE_RECURSIVE_MACRO_CALLS.get(configuration))
+            .build();
 
         Jinjava jinjava = new Jinjava(config);
+
+        List<String> locations = RESOURCE_LOCATIONS_CALLS.getOptional(configuration).orElse(List.of());
+        if (!locations.isEmpty()) {
+            List<ResourceLocator> locators = locations.stream().map(baseDir -> {
+                try {
+                    return (ResourceLocator) new FileLocator(new File(baseDir));
+                } catch (FileNotFoundException e) {
+                    LOG.warn("Cannot configure Jinja resource location: '{}'. Cause: {}.", baseDir, e.getMessage());
+                    return null;
+                }
+            }).filter(Objects::nonNull).collect(toCollection(ArrayList::new));
+            locators.add(new ClasspathResourceLocator());
+            jinjava.setResourceLocator(new CascadingResourceLocator(locators.toArray(new ResourceLocator[0])));
+        }
         Map<String, Object> bindingsMap = buildBindingsMapFrom(bindings, location);
         RenderResult result = jinjava.renderForResult(template, bindingsMap);
 
@@ -103,13 +133,13 @@ public class JinjaResourceTemplateRenderer implements ResourceTemplateRenderer {
         if (!errors.isEmpty()) {
             TemplateError error = errors.getFirst();
             throw new JikkouRuntimeException(
-                    String.format(
-                            "Cannot render resource template. '%s': line %d, start_pos: %d, %s",
-                            formatErrorReason(error.getReason().name()),
-                            error.getLineno(),
-                            error.getStartPosition(),
-                            error.getMessage()
-                    )
+                String.format(
+                    "Cannot render resource template. '%s': line %d, start_pos: %d, %s",
+                    formatErrorReason(error.getReason().name()),
+                    error.getLineno(),
+                    error.getStartPosition(),
+                    error.getMessage()
+                )
             );
         }
         LOG.debug("Resource template rendering done");
@@ -138,9 +168,9 @@ public class JinjaResourceTemplateRenderer implements ResourceTemplateRenderer {
         if (location != null) {
             Path path = Paths.get(location.getPath());
             bindingsMap.put(Scopes.RESOURCE.key(), Map.of(
-                "path", path,
-                "name", path.getFileName().toString(),
-                "directoryPath", Optional.ofNullable(path.getParent()).map(Path::toAbsolutePath).map(Path::toString).orElse("")
+                    "path", path,
+                    "name", path.getFileName().toString(),
+                    "directoryPath", Optional.ofNullable(path.getParent()).map(Path::toAbsolutePath).map(Path::toString).orElse("")
                 )
             );
         }
@@ -153,8 +183,8 @@ public class JinjaResourceTemplateRenderer implements ResourceTemplateRenderer {
      */
     private static String formatErrorReason(final String s) {
         String formatted = Pattern.compile("_([a-z])")
-                .matcher(s.toLowerCase())
-                .replaceAll(m -> m.group(1).toUpperCase());
+            .matcher(s.toLowerCase())
+            .replaceAll(m -> m.group(1).toUpperCase());
         return formatted.substring(0, 1).toUpperCase() + formatted.substring(1);
     }
 
